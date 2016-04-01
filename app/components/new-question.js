fc85824c3d951245b744c724b96547b19b0dcf0f
Mary Warrington <marywarrington@gmail.com>
2016-04-01 08:50:43 -0700

    Add new-question component

---
+++ app/components/new-question.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+
+    save() {
+      var params = {
+        author: this.get('author') ? this.get('author') : "",
+        question: this.get('question') ? this.get('question') : "",
+        extra: this.get('extra') ? this.get('extra') : "",
+      };
+      this.set('addNewQuestion', false),
+      this.sendAction('save', params);
+    }
+  }
+});
